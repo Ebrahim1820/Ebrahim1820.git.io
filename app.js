4997a32015ebc6990ba921516d090dcfc8c8afaa
Ebrahim <naser.najafi1988@gmail.com>
2023-08-30 00:03:07 +0200

    update and rewrite the code for fs-sync.js in app file, to see how much is short and cleaner is it

--- app.js
+++ app.js
@@ -1,22 +1,80 @@
-const http = require('http')
+// npm run dev / npm start
 
-const server = http.createServer((req, res)=>{
-    if(req.url === '/'){
-        res.end('Welcome to home page !')
-    }
-    if(req.url === '/about'){
-        res.end('Welcome to about page')
-    }
-    if(req.url==='/error'){
-        res.end(`
-        <h1> Oops! something goes wrong </h1>
-    
-        <p> We can't seem to find the page you are looking for</p>
-        <a href="/" text-color: blue> Home page </a>
-        `
-       )
-    }
-   
-})
+const fs = require('fs').promises
+  
+const start = async()=>{
+    try{
+        const firstFile =await fs.readFile('./content/first.txt', 'utf8')
+        const secondFile =await fs.readFile('./content/second.txt','utf8')
+        await fs.writeFile('./content/result-more-upadet-code-sync.txt',`This is Updated code and clean code: ${firstFile} ${secondFile}`)
+        console.log(firstFile)
+        console.log(secondFile)
+    }catch(error){
 
-server.listen(5000)
+        console.log(error)
+
+    }
+}
+
+start()
+
+
+
+// const getText = (path)=>{
+//     return new Promise((resolve, reject)=>{
+      
+//         fs.readFile(path, 'utf8', (error, data)=>{
+//             if(error){
+//                 reject(error)
+//             }else{
+//                 resolve(data)    
+//             }    
+//         })  
+//     })
+// }
+
+// getText('../content/first.txt')
+// .then((result)=>console.log(result))
+// .catch((error)=>console.log(error))
+
+// const start = async()=>{
+//     try{
+//         const firstFile =await getText('./content/first.txt')
+//         const secondFile =await getText('./content/second.txt')
+//         console.log(firstFile)
+//         console.log(secondFile)
+//     }catch(error){
+
+//         console.log(error)
+
+//     }
+// }
+
+// start()
+
+
+///////////////////// more clener
+
+
+// const util = require('util')
+// const readFilePromis = util.promisify(fs.readFile)  
+// const writeFilePromis = util.promisify(fs.writeFile)  
+
+
+
+
+
+// const start = async()=>{
+//     try{
+//         const firstFile =await readFilePromis('./content/first.txt', 'utf8')
+//         const secondFile =await readFilePromis('./content/second.txt','utf8')
+//         await writeFilePromis('./content/result-upadet-code-sync.txt',`This is Updated code and clean code: ${firstFile} ${secondFile}`)
+//         console.log(firstFile)
+//         console.log(secondFile)
+//     }catch(error){
+
+//         console.log(error)
+
+//     }
+// }
+
